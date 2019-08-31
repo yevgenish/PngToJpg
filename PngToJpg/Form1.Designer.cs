432c64990bbc6c2439458f859e308d1dea56e34e
Yevgeni <Yevgeni@DESKTOP-BIS8V8E>
2019-09-01 02:47:39 +0300

    Initial commit

---
+++ PngToJpg/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PngToJpg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(53, 69);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(53, 38);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(377, 20);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(53, 13);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(39, 13);
+            this.lblFolder.TabIndex = 2;
+            this.lblFolder.Text = "Folder:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 338);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "PNG to JPG Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label lblFolder;
+    }
+}
+
---
+++ PngToJpg/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PngToJpg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string folder = txtFolder.Text;
+
+                string errorMessage = null;
+                bool isPathValid = ValidatePath(folder, out errorMessage);
+
+                if (!isPathValid)
+                {
+                    MessageBox.Show(errorMessage);
+                    return;
+                }
+
+                int filesConverted = ToJPG(folder);
+
+                MessageBox.Show(String.Format("Done. Files converted: {0}.", filesConverted));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public int ToJPG(string folder)
+        {
+            int filesConverted = 0;
+            foreach (string file in Directory.GetFiles(folder))
+            {
+                string extension = Path.GetExtension(file);
+                if (extension.ToLower() == ".png")
+                {
+                    string name = Path.GetFileNameWithoutExtension(file);
+                    string path = Path.GetDirectoryName(file);
+                    using(Image png = Image.FromFile(file))
+                    {
+                        string newFilePath = Path.Combine(path, name + ".jpg");
+                        png.Save(newFilePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        filesConverted++;
+                    }
+                }
+            }
+
+            return filesConverted;
+        }
+
+        private bool ValidatePath(string folder, out string errorMessage)
+        {
+            errorMessage = null;
+
+            try
+            {
+                if (String.IsNullOrWhiteSpace(folder))
+                {
+                    errorMessage = "Empty folder.";
+                    return false;
+                }
+
+                string folderPath = Path.GetFullPath(folder);
+
+                if (folderPath.Equals(folder))
+                {
+                    return true;
+                }
+                else
+                {
+                    errorMessage = "Folder is invalid.";
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.ToString();
+            }
+
+            return false;
+        }
+
+     
+    }
+}
